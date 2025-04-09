efedffeec94f4caafba4b29a76b180d46754b1ce
Coder <vasa1252@yandex.ru>
2025-04-09 12:19:16 +0300

    moded functions widget and masks

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,10 @@
-def get_mask_card_number(number_card: str) -> str:
+def get_mask_card_number(number_card: str, index: int) -> str:
     """Функция которая прячет часть номера карты"""
 
-    return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[-4:]}"
+    return f"{number_card[:index-1]} {number_card[index:index+4]} {number_card[index+4:index+6]}** **** {number_card[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """ "Функция которая прячет часть номера счета"""
 
-    return f"**{str(account_number[-4:])}"
+    return f"{account_number[:4]} **{account_number[-4:]}"
--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,17 @@
+import masks
+
 def mask_account_card(card: str) -> str:
     """Функция которая прячет часть номера карты или счета"""
 
     index = 0
     if card[0] == "С":
-        return f"{card[:4]} **{card[-4:]}"
+        return masks.get_mask_account(card)
     else:
         for i in range(len(card)):
             if card[i].isdigit():
                 index = i
                 break
-        return f"{card[:index-1]} {card[index:index+4]} {card[index+4:index+6]}** **** {card[-4:]}"
+        return masks.get_mask_card_number(card, index)
+
+
+print(mask_account_card("Maestro 1596837868705199"))
