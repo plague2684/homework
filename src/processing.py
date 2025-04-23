d7ef8b47a2bfb1ad873863396941908e56eecde6
Coder <vasa1252@yandex.ru>
2025-04-23 14:17:22 +0300

    add README.md and moded processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,15 @@
-def filter_by_state(info: list, state='EXECUTED') -> list:
+def filter_by_state(info: list, state="EXECUTED") -> list:
     """Функция которая возвращяет только те значения, которые подходят под критерий"""
     answer = []
     for i in info:
         if i["state"] == state:
             answer.append(i)
+
     return answer
 
 
 def sort_by_date(info: list, reversed=False) -> list:
     """Функция которая сортирует по дате"""
-    answer = sorted(info, key=lambda s: s['date'],reverse= reversed)
+    answer = sorted(info, key=lambda s: s["date"], reverse=reversed)
+
     return answer
