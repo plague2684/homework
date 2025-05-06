c8da3c9304263a8aa861c007e1f80bad03da7f69
Coder <vasa1252@yandex.ru>
2025-05-06 15:53:54 +0300

    moded files directories src,tests

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,8 @@
 def get_mask_card_number(number_card: str, index: int) -> str:
     """Функция которая прячет часть номера карты"""
 
-    return f"""{number_card[:index-1]} {number_card[index:index+4]} 
-{number_card[index+4:index+6]}** **** {number_card[-4:]}"""
+    return (f"{number_card[:index-1]} {number_card[index:index+4]} "
+            f"{number_card[index+4:index+6]}** **** {number_card[-4:]}")
 
 
 def get_mask_account(account_number: str) -> str:
--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,15 @@
-import masks
-
-
 def mask_account_card(card: str) -> str:
     """Функция которая прячет часть номера карты или счета"""
-
+    from src.masks import get_mask_account, get_mask_card_number
     index = 0
     if card[0] == "С":
-        return masks.get_mask_account(card)
+        return get_mask_account(card)
     else:
         for i in range(len(card)):
             if card[i].isdigit():
                 index = i
                 break
-        return masks.get_mask_card_number(card, index)
+        return get_mask_card_number(card, index)
 
 
 def get_date(date: str) -> str:
