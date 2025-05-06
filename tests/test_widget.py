4a0b29ece29a82157303d665aa1c96cf3ef8b2b6
Coder <vasa1252@yandex.ru>
2025-05-06 16:04:30 +0300

    moded test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,14 +1,29 @@
-import pytest
 from src.widget import get_date, mask_account_card
-from src.masks import get_mask_account, get_mask_card_number
+import pytest
 
 
-def test_widget_date():
+@pytest.fixture
+def date():
+    return "2024-03-11T02:26:18.671407"
 
-    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
 
-def test_widget_number():
+@pytest.fixture
+def card():
+    return "Visa Platinum 7000792289606361"
 
-    assert mask_account_card("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
 
-    assert mask_account_card("Счет 73654108430135874305") == 'Счет **4305'
+@pytest.fixture
+def account():
+    return "Счет 73654108430135874305"
+
+
+def test_widget_date(date):
+
+    assert get_date(date) == "11.03.2024"
+
+
+def test_widget_number(card, account):
+
+    assert mask_account_card(card) == "Visa Platinum 7000 79** **** 6361"
+
+    assert mask_account_card(account) == "Счет **4305"
