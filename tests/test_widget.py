015b001b49ceb4f73b8e6ccab020a360c39e87f0
Coder <vasa1252@yandex.ru>
2025-05-06 16:12:08 +0300

    added parametrize

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -7,23 +7,18 @@ def date():
     return "2024-03-11T02:26:18.671407"
 
 
-@pytest.fixture
-def card():
-    return "Visa Platinum 7000792289606361"
-
-
-@pytest.fixture
-def account():
-    return "Счет 73654108430135874305"
-
-
 def test_widget_date(date):
 
     assert get_date(date) == "11.03.2024"
 
 
-def test_widget_number(card, account):
+@pytest.mark.parametrize('value, answer', [
+    ('Visa Platinum 7000792289606361', "Visa Platinum 7000 79** **** 6361"),
+    ('Счет 73654108430135874305', "Счет **4305")
+])
 
-    assert mask_account_card(card) == "Visa Platinum 7000 79** **** 6361"
 
-    assert mask_account_card(account) == "Счет **4305"
+def test_widget_number(value, answer):
+
+    assert mask_account_card(value) == answer
+
